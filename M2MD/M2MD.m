a11c6df69ea25a293863f823877f3fa515bcb273
Kuba <kuba.pod@gmail.com>
2018-05-24 14:11:10 +0200

    structure

---
+++ M2MD/M2MD.m
@@ -0,0 +1,44 @@
+
+(* ::Package:: *)
+
+(* ::Chapter:: *)
+(* Metadata*)
+
+(* Mathematica Package *)
+
+(* :Title: M2MD *)
+(* :Context: M2MD` *)
+(* :Author: Kuba (kuba.pod@gmail.com) *)
+(* :Date: Thu 24 May 2018 12:44:21 *)
+
+(* :Keywords: *)
+(* :Discussion: *)
+
+(* ::Chapter:: *)
+(* Begin package*)
+
+
+BeginPackage["M2MD`"];
+
+Unprotect["`*", "`*`*"]
+ClearAll["`*", "`*`*"]
+
+(* Needs and exported symbols here *)
+
+Begin["`Private`"];
+
+(* ::Chapter:: *)
+(* Implementation code*)
+
+
+(*implementation goes here*)
+
+
+
+(* ::Chapter:: *)
+(* End package*)
+
+
+End[];
+
+EndPackage[];
---
+++ M2MD/PacletInfo.m
@@ -0,0 +1,5 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+Paclet[Name -> "M2MD", Version -> "0.0.1", MathematicaVersion -> "10.4+", 
+ Description -> "Notebook to Markdown converter", 
+ Creator -> "Kuba (kuba.pod@gmail.com)", 
+ Extensions -> {{"Kernel", Context -> "M2MD`"}, {"FrontEnd"}}]
