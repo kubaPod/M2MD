d735c4daa73dccf82f17616c1e16fcfc5d4e5ac1
Kuba <kuba.pod@gmail.com>
2018-05-24 16:34:15 +0200

    feat(dev) build script

---
+++ build.m
@@ -0,0 +1,47 @@
+(* ::Package:: *)
+
+BeginPackage["M2MDBuild`"];
+
+
+Needs["M2MD`"];
+
+
+$project = DirectoryName[$InputFileName /. "" :> NotebookFileName[]];
+$projectName = FileBaseName @ $project;
+
+M2MDBild;
+
+Begin["`Private`"];
+
+M2MDBild[]:= buildMainPalette[];
+
+buildMainPalette[]:=Module[{nb}
+  , nb = mainPalette[]
+  ; NotebookSave[
+    nb
+    , FileNameJoin[{$project, $projectName, "FrontEnd", "Palettes", $projectName <> ".nb"}]
+  ]
+  ; NotebookClose[nb]
+]
+
+mainPalette[]:= CreatePalette[
+  Button[
+    "Export to Markdown"
+  , Needs["M2MD`"]
+  ; CreateDocument[
+      Cell[
+        M2MD @ InputNotebook[]
+      , "Program"
+      ]
+    ]
+  , Method -> "Queued"  
+  , ImageSize -> CurrentValue @ "DefaultButtonSize"
+  ]
+, WindowTitle -> $projectName
+];
+
+
+
+
+
+End[];
