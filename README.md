a9b0f93242b097c64ec76b8d62822d0c070ff978
Kuba Podkalicki <kuba.pod@gmail.com>
2018-05-25 07:31:16 +0200

    commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # M2MD
+
+Simple converter of Mathematica notebooks to markdown.
--- build.m
+++ build.m
@@ -6,42 +6,43 @@ BeginPackage["M2MDBuild`"];
 Needs["M2MD`"];
 
 
-$project = DirectoryName[$InputFileName /. "" :> NotebookFileName[]];
-$projectName = FileBaseName @ $project;
+  $project = DirectoryName[$InputFileName /. "" :> NotebookFileName[]];
+  $projectName = FileBaseName @ $project;
 
-M2MDBild;
+  M2MDBild;
 
 Begin["`Private`"];
 
-M2MDBild[]:= buildMainPalette[];
+  M2MDBild[]:= buildMainPalette[];
 
-buildMainPalette[]:=Module[{nb}
-  , nb = mainPalette[]
-  ; NotebookSave[
-    nb
-    , FileNameJoin[{$project, $projectName, "FrontEnd", "Palettes", $projectName <> ".nb"}]
-  ]
-  ; NotebookClose[nb]
-]
-
-mainPalette[]:= CreatePalette[
-  Button[
-    "Export to Markdown"
-  , Needs["M2MD`"]
-  ; CreateDocument[
-      Cell[
-        M2MD @ InputNotebook[]
-      , "Program"
-      ]
+  buildMainPalette[]:=Module[{nb}
+    , nb = mainPalette[]
+    ; NotebookSave[
+      nb
+      , FileNameJoin[{$project, $projectName, "FrontEnd", "Palettes", $projectName <> ".nb"}]
     ]
-  , Method -> "Queued"  
-  , ImageSize -> CurrentValue @ "DefaultButtonSize"
+    ; NotebookClose[nb]
   ]
-, WindowTitle -> $projectName
-];
+
+  mainPalette[]:= CreatePalette[
+    Button[
+      "Export to Markdown"
+    , Needs["M2MD`"]
+    ; CreateDocument[
+        Cell[
+          M2MD @ InputNotebook[]
+        , "Program"
+        ]
+      ]
+    , Method -> "Queued"
+    , ImageSize -> CurrentValue @ "DefaultButtonSize"
+    ]
+  , WindowTitle -> $projectName
+  ];
 
 
 
 
 
 End[];
+EndPackage[];
