c7d510ecd83b662799a61796277858f8f5f70cd6
Kuba Podkalicki <kuba.pod@gmail.com>
2019-01-07 13:46:15 +0100

    paclet site for redirect

--- PacletSite.m
+++
@@ -1,9 +0,0 @@
-(* Created with the Wolfram Language : www.wolfram.com *)
-PacletSite[Paclet["Name" -> "M2MD", "Version" -> "0.1.0", 
-  "MathematicaVersion" -> "10.4+", "Description" -> 
-   "Notebook to Markdown converter", 
-  "Creator" -> "Kuba (kuba.pod@gmail.com)", 
-  "Extensions" -> {{"Kernel", "Context" -> "M2MD`"}, {"FrontEnd"}}, 
-  "Location" -> 
-   "http://github.com/kubaPod/M2MD/releases/download/v0.1.0/M2MD-0.1.0.paclet\
-"]]
