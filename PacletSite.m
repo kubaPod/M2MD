6c8631878f3aa542d3c6b2f941bd9336ee17e5bb
Kuba <kuba.pod@gmail.com>
2019-01-07 13:31:37 +0100

    location

--- PacletSite.m
+++ PacletSite.m
@@ -4,4 +4,6 @@ PacletSite[Paclet["Name" -> "M2MD", "Version" -> "0.1.0",
    "Notebook to Markdown converter", 
   "Creator" -> "Kuba (kuba.pod@gmail.com)", 
   "Extensions" -> {{"Kernel", "Context" -> "M2MD`"}, {"FrontEnd"}}, 
-  "Location" -> "http://github.com/kubaPod/M2MD/releases/download/v0.1.0/"]]
+  "Location" -> 
+   "http://github.com/kubaPod/M2MD/releases/download/v0.1.0/M2MD-0.1.0.paclet\
+"]]
