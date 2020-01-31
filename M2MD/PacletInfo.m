2ea8c1d3628a14beeafdc82c33dab599efc30389
Kuba Podkalicki <kuba.pod@gmail.com>
2020-01-31 13:32:54 +0100

    0.5.0

--- M2MD/PacletInfo.m
+++ M2MD/PacletInfo.m
@@ -1,5 +1,5 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
-Paclet[Name -> "M2MD", Version -> "0.4.0", MathematicaVersion -> "10.4+", 
+Paclet[Name -> "M2MD", Version -> "0.5.0", MathematicaVersion -> "10.4+", 
  Description -> "Notebook to Markdown converter", 
  Creator -> "Kuba (kuba.pod@gmail.com)", 
  Extensions -> {{"Kernel", Context -> "M2MD`"}, {"FrontEnd"}}]
